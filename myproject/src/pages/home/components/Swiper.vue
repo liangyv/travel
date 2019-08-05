6674aaa9f96eae7a8712ca1b6febc50963094087
liangyv <1531197646@qq.com>
2019-08-05 08:26:13 +0800

    swiper

--- myproject/src/pages/home/components/Swiper.vue
+++ myproject/src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper">
         <swiper :options="swiperOption">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <swiper-slide v-for="item of swiperList" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" alt="">
     </swiper-slide>
    
@@ -32,8 +32,6 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-.wrapper>>> .swiper-pagination-bullet-active
-        background :red !important
 .wrapper
     width:100%
     height:0
